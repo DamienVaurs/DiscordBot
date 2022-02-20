27ffde20af98975d5881b110877a97b9fa29bc5b
Damien V <100078122+DamienVaurs@users.noreply.github.com>
2022-02-20 13:59:35 +0000

    Messages de connexion

--- bot.py
+++ bot.py
@@ -1,3 +1,19 @@
 import discord
 
+
+client = discord.Client()
+
+@client.event
+async def on_ready():
+    print('We have logged in as {0.user}'.format(client))
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if message.content.startswith('$hello'):
+        await message.channel.send('Hello!')
+
+
 client.run('NDQ0NDYwMDg0NTUxMjIxMjQ4.WvV9tQ.3zxA6Ld089gi6mNYc56-Zw8ouxQ')
